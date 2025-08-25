34b8c53e98a615ea7defca3fe60c9fff8a09c6df
yaronblum <blumyaron@gmail.com>
2025-08-25 18:54:48 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,17 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+class ElementsUtils:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def wait_for_element_to_clickable(self, locator_type, locator_value):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((locator_type, locator_value))
+            )
+        except Exception as err:
+            raise Exception(f"failed to properly wait for element-clickable: {err}")
+
+        return element
